234c2e65f01e90a3fab71165b505817a4c650d1d
Islomkhuja <nanotexnolagiya@gmail.com>
2021-03-24 19:16:42 +0500

    feat: Add searchable selectbox

--- src/user/components/UserForm.tsx
+++ src/user/components/UserForm.tsx
@@ -1,6 +1,6 @@
-import { Button, Col, Form, Input, Row } from 'antd';
+import { Button, Col, Form, Input, Row, Select, Spin } from 'antd';
+import { FC, useState } from 'react';
 
-import { FC } from 'react';
 import { RouteChildrenProps } from 'react-router';
 
 export interface UserFormProps extends RouteChildrenProps {}
@@ -10,12 +10,50 @@ export const UserForm: FC<UserFormProps> = ({ history }) => {
     history.goBack();
   };
 
+  const [loading, setLoading] = useState(false);
+  const [options, setOptions] = useState([]);
+
+  const handleChange = () => {};
+
+  const handleSearch = () => {};
+
+  const handleScrollPopup = () => {};
+
   return (
     <Form layout="vertical">
       <Row gutter={[15, 15]}>
         <Col span={24}>
-          <Form.Item name="name" label="Name">
+          <Form.Item
+            name="name"
+            label="Name"
+            rules={[{ required: true, message: 'Field required' }]}
+          >
             <Input />
+          </Form.Item>
+        </Col>
+        <Col span={24}>
+          <Form.Item
+            className="defaultInput"
+            name="role_id"
+            label={'Role'}
+            rules={[{ required: true, message: 'Field required' }]}
+          >
+            <Select
+              onChange={handleChange}
+              onSearch={handleSearch}
+              onPopupScroll={handleScrollPopup}
+              placeholder={'Choose Role'}
+              showSearch
+              notFoundContent={loading ? <Spin size="small" /> : null}
+              loading={loading}
+              filterOption={false}
+            >
+              {options.map((option: any) => (
+                <Select.Option key={option.id} value={option.id} item={option}>
+                  {option.name}
+                </Select.Option>
+              ))}
+            </Select>
           </Form.Item>
         </Col>
       </Row>
